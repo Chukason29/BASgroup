b8fe8f44ef26faf17212114870d078c409515663
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-10-15 19:50:01 +0100

    Making the vertical section slider

--- js/app.js
+++ js/app.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
-	$('#pagepiling').pagepiling({
-	    menu: null,
-        direction: 'vertical',
-        verticalCentered: true,
-        sectionsColor: [],
-        anchors: [],
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: true,
-        loopTop: true,
-        css3: true,
-        navigation: {
-            'textColor': '#000',
-            'bulletsColor': '#000',
-            'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4']
-        },
-       	normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
+	if (matchMedia('only screen and (min-width: 768px)').matches) {
+		$('#pagepiling').pagepiling({
+			menu: null,
+			direction: 'vertical',
+			verticalCentered: true,
+			sectionsColor: [],
+			anchors: [],
+			scrollingSpeed: 700,
+			easing: 'swing',
+			loopBottom: true,
+			loopTop: true,
+			css3: true,
+			navigation: {
+				'textColor': '#000',
+				'bulletsColor': '#000',
+				'position': 'right',
+				'tooltips': ['section1', 'section2', 'section3', 'section4']
+			},
+			normalScrollElements: null,
+			normalScrollElementTouchThreshold: 5,
+			touchSensitivity: 5,
+			keyboardScrolling: true,
+			sectionSelector: '.section',
+			animateAnchor: false,
 
-		//events
-		onLeave: function(index, nextIndex, direction){},
-		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
-	});
+			//events
+			onLeave: function(index, nextIndex, direction){},
+			afterLoad: function(anchorLink, index){},
+			afterRender: function(){},
+		});
+	}
 });
+
+
